87ebbe0f175a95fe7e5290871c7bc9f5337ee251
vipulchavan47 <vipulchavan3301@outlook.com>
2024-08-08 20:34:07 +0530

    arrays added

---
+++ Arrays/Easy/RemoveDups.java
@@ -0,0 +1,15 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//        int j=1;
+
+//        for(int i=1;i<nums.length;i++){
+//             if(nums[i] != nums[i-1]){
+//                 nums[j]=nums[i];
+//                 j++;
+//             }
+//        }
+//         return j;
+//     }
+// }
+
+// Two pointers method first item in the array always be unique so we have to move from 2nd position then we will check if nums[1] != nums[2] if yes then we will modify the nums array by putting the nums[j]=nums[i] (with that unique element)
---
+++ Arrays/Easy/secondLargest.java
@@ -0,0 +1,18 @@
+
+// class Solution {
+// int print2largest(int arr[], int n) {
+// // code here
+// int largest=arr[0],secLargest=-1;
+// for(int i=0;i<n;i++){
+// if(arr[i]>largest){
+// secLargest=largest;
+// largest=arr[i];
+// }
+// if value less than largest but its greater than second largest then
+// else if(arr[i]<largest && arr[i]>secLargest){
+// secLargest=arr[i];
+// }
+// }
+// return secLargest;
+// }
+// }
