a1a9427878c40fb2f281d487e5c42357e3253e32
vipulchavan47 <vipulchavan3301@outlook.com>
2024-08-12 20:43:44 +0530

    arrays easy sloved

---
+++ Arrays/Easy/SingleNumber.java
@@ -0,0 +1,28 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+
+import java.util.HashMap;
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
